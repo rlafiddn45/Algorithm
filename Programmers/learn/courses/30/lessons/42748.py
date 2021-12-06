4d5c42af6174fbfa418db6cb6bf42ccbedf07682
rlafiddn45 <rlafiddn45@gmail.com>
2021-12-07 00:51:31 +0900

    20211206 commit

---
+++ Programmers/learn/courses/30/lessons/42748.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        temp_array = array[command[0] - 1:command[1]]
+        temp_array.sort()
+        answer.append(temp_array[command[2] - 1])
+    return answer
+
