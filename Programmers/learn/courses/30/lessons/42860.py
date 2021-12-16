03539b9f8c075777d1aa0b77de40d7227d2a4303
rlafiddn45 <rlafiddn45@gmail.com>
2021-12-17 01:04:11 +0900

    20211216 commit

--- Programmers/learn/courses/30/lessons/42860.py
+++ Programmers/learn/courses/30/lessons/42860.py
@@ -1,21 +1,43 @@
+from collections import deque
+
+
 def solution(name):
     answer = 0
-    alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     for n in name:
-        temp = alphabets.index(n) - 0
+        temp = ord(n) - ord('A')
         answer += min(temp, 26 - temp)
-    not_as = [i for i in range(1, len(name)) if name[i] != 'A']
-    item = 0
-    while len(not_as) > 0:
-        temp = not_as.pop()
-        answer += min(abs(temp - item), len(name) - temp + item)
-        print('temp, item, answer : ', temp, item, answer)
-        item = temp
+    arr = deque([i for i in range(len(name)) if name[i] != 'A'])
+    idx = 0
+    length = len(name)
+    while arr:
+        if idx <= arr[0]:
+            if arr[0] - idx <= length - arr[-1] + idx:
+                temp = arr.popleft()
+                answer += temp - idx
+                idx = temp
+            else:
+                temp = arr.pop()
+                answer += length - temp + idx
+                idx = temp
+        elif idx >= arr[-1]:
+            if idx - arr[-1] <= length - idx + arr[0]:
+                temp = arr.pop()
+                answer += idx - temp
+                idx = temp
+            else:
+                temp = arr.popleft()
+                answer += length - idx + temp
+                idx = temp
+
     return answer
 
 
+print(solution("ZZAAAZZ"))
+# print(solution("ZZZAAAZ"))
 # print(solution("JEROEN"))
-print(solution("BBAlB"))
+# print(solution("BBAAAAAAAB"))
 # print(solution("A"))
 # print(solution("AAAAAAA"))
 # print(solution("ABC"))
+# print(solution("AAABA"))
+
