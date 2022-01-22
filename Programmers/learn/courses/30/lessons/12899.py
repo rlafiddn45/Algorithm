deccc81c6bf194bca80ab8aae3f2c86449980b9c
rlafiddn45 <rlafiddn45@gmail.com>
2022-01-22 17:07:19 +0900

    20220122 commit

---
+++ Programmers/learn/courses/30/lessons/12899.py
@@ -0,0 +1,17 @@
+def solution(n):
+    answer = ''
+    while n > 0:
+        if n % 3 != 0:
+            answer += str(n % 3)
+            n //= 3
+        else:
+            answer += '4'
+            n = n // 3 - 1
+    return answer[::-1]
+
+
+print(solution(1))
+print(solution(2))
+print(solution(3))
+print(solution(4))
+
