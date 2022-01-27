3fbf877694d867fb2833919508e98897e6eac5f4
rlafiddn45 <rlafiddn45@gmail.com>
2022-01-28 00:16:01 +0900

    20220128 commit

---
+++ BackJoon/2164.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+
+N = int(input())
+cards = deque([i for i in range(1, N + 1)])
+while len(cards) > 1:
+    cards.popleft()
+    cards.append(cards.popleft())
+
+print(cards[0])
+
