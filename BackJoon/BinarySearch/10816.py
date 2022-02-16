0e3610fc90b0389a4f4f7a750c42d2176ff8f2db
rlafiddn45 <rlafiddn45@gmail.com>
2022-02-17 00:13:56 +0900

    20220217 commit

--- BackJoon/BinarySearch/10816.py
+++ BackJoon/BinarySearch/10816.py
@@ -1,90 +1,35 @@
-# import sys
-# N = int(sys.stdin.readline())
-# arr_N = list(map(int, sys.stdin.readline().split()))
-# M = int(sys.stdin.readline())
-# arr_M = list(map(int, sys.stdin.readline().split()))
-#
-#
-# def binary_search(target, src):
-#     answer = []
-#     target.sort()
-#
-#     for s in src:
-#         check, start, end, mid = 0, 0, len(target) - 1, 0
-#
-#         while start <= end:
-#             mid = (start + end) // 2
-#             if s == target[mid]:
-#                 check = 1
-#                 break
-#             elif s < target[mid]:
-#                 end = mid - 1
-#             elif s > target[mid]:
-#                 start = mid + 1
-#
-#         cnt, left, right = 1, mid - 1, mid + 1
-#         if not check:
-#             cnt = 0
-#         else:
-#             while left >= 0 and target[left] == target[mid]:
-#                 cnt, left = cnt + 1, left - 1
-#             while right < len(target) and target[right] == target[mid]:
-#                 cnt, right = cnt + 1, right + 1
-#
-#         answer.append(cnt)
-#
-#     return answer
-#
-#
-# res = binary_search(arr_N, arr_M)
-# print(' '.join(map(str, res)))
-#
-# '''
-# 5
-# 1 1 1 1 1
-# 4
-# 1 2 3 4
-# '''
-
-
 import sys
 N = int(sys.stdin.readline())
-arr_N = list(map(int, sys.stdin.readline().split()))
+arr_n = list(map(int, sys.stdin.readline().split()))
 M = int(sys.stdin.readline())
-arr_M = list(map(int, sys.stdin.readline().split()))
+arr_m = list(map(int, sys.stdin.readline().split()))
 
 
-def binary_search(target, src):
-    answer = []
-    target.sort()
+arr_n.sort()  # 이분 탐색을 위해 정렬
+answer = []
 
-    for s in src:
-        cnt, start, end, mid = 0, 0, len(target) - 1, 0
-
-        while start <= end:
-            mid = (start + end) // 2
-            print('s, start, end, mid :', s, start, end, mid)
-            if s == target[mid]:
-                print('s, target[mid + 1] :', s, target[mid + 1])
-                cnt += 1
-                left, right = mid - 1, mid + 1
-                while left >= start and target[left] == s:
-                    cnt, left = cnt + 1, left - 1
-                while right <= end and target[right] == s:
-                    cnt, right = cnt + 1, right + 1
-                break
-            elif s < target[mid]:
-                end = mid - 1
-            elif s > target[mid]:
-                start = mid + 1
-
-        answer.append(cnt)
-
-    return answer
+for m in arr_m:
+    left, right = 0, N - 1
+    x, y = 0, 0
+    while left <= right:
+        mid = (left + right) // 2
+        if arr_n[mid] >= m:
+            right = mid - 1
+        elif arr_n[mid] < m:
+            left = mid + 1
+    x = left
+    left, right = 0, N - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr_n[mid] <= m:
+            left = mid + 1
+        elif arr_n[mid] > m:
+            right = mid - 1
+    y = left
+    answer.append(y - x)
 
 
-res = binary_search(arr_N, arr_M)
-print(' '.join(map(str, res)))
+print(' '.join(map(str, answer)))
 
 '''
 5
@@ -98,3 +43,12 @@ print(' '.join(map(str, res)))
 
 10 9 -5 2 3 4 5 -10
 '''
+
+'''
+8
+10 9 -5 2 3 4 5 -10
+8
+10 9 -5 2 3 4 5 -10
+'''
+
+# -10 -5 2 3 4 5 9 10
